aebe0795bc51f334d539582e3506105853392c5b
PWithanage <psathsaranee@gmail.com>
2017-11-22 22:42:50 +0530

    edit supplier modifications

--- src/app/dashboard/supplier/view-supplier/view-supplier.component.ts
+++ src/app/dashboard/supplier/view-supplier/view-supplier.component.ts
@@ -10,27 +10,29 @@ import 'rxjs/add/operator/map';
 })
 export class ViewSupplierComponent implements OnInit {
 
-  suppliers : any = [];
-  
-  constructor(private http: Http) { }
+  suppliers: any = [];
 
+  constructor(private http: Http) { }
+  //this function is automatically called when the specific page is loaded
   ngOnInit() {
-    this.getData();
+    //calling the two functions to extract data
+    this.getSupplierData();
     this.getSupplier();
   }
 
-  //getting data from the get request
-  getData(){
+  //getting Supplier data from the GET request
+  getSupplierData() {
     return this.http.get('http://localhost:4000/procument/supplier')
-    .map((res: Response)=>res.json())
+      .map((res: Response) => res.json())
   }
 
-    //adding data to supplier variable (defined upper) which are got from getdata() 
-    getSupplier(){
-      this.getData().subscribe(supplier=> {
-        console.log(supplier);       
-        this.suppliers = supplier
-      })
-    }
+  //adding data to suppliers variable (defined above)
+  getSupplier() {
+    this.getSupplierData().subscribe(supplier => {
+      console.log(supplier);
+      this.suppliers = supplier
+    })
+  }
+
 
 }
