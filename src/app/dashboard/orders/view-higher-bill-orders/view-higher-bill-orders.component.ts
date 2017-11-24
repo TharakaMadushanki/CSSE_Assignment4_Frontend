f884375df2bd381b5e3fc65f09f2f90ccceca9ae
PWithanage <psathsaranee@gmail.com>
2017-11-25 04:48:07 +0530

    higher bill orders

--- src/app/dashboard/orders/view-higher-bill-orders/view-higher-bill-orders.component.ts
+++ src/app/dashboard/orders/view-higher-bill-orders/view-higher-bill-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-view-higher-bill-orders',
@@ -7,9 +9,127 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewHigherBillOrdersComponent implements OnInit {
 
-  constructor() { }
+  suppliers: any = [];
+  orders: any = [];
+  type: String;
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.getSupplier();
+    this.getOrder();
+  }
+  
+  getSupplier() {
+    this.http.get('http://localhost:4000/procument/supplier')
+      .map((res: Response) => res.json()).subscribe(supplier => {
+        console.log(supplier);
+        this.suppliers = supplier
+      })
+  }
+
+  getOrder() {
+    this.type = "HigherBill";
+    this.http.get('http://localhost:4000/procument/placeOrder/orderType/' + this.type)
+      .map((res: Response) => res.json()).subscribe(order => {
+        console.log(this.type)
+        console.log(order);
+        this.orders = order
+      })
+  }
+
+  public onChangeStatus(event): void {
+    const status = event.target.value;
+    console.log(status);
+    if (status == null || status == "") {
+      this.getOrder();
+    }
+    else
+      this.getHigherBillOrderDataByStatus(status);
+  }
+
+  getHigherBillOrderDataByStatus(status: String) {
+    this.type = "HigherBill";
+    return this.http.get('http://localhost:4000/procument/placeOrder/' + this.type + '/status/' + status)
+      .map((res: Response) => res.json()).subscribe(order => {
+        console.log(order);
+        this.orders = order
+      })
+  }
+
+  public onChangeSupplier(event): void {
+    const supplier = event.target.value;
+    console.log(supplier);
+    if (supplier == null || supplier == "") {
+      this.getOrder();
+    }
+    else
+      this.getHigherBillOrderDataBySupplier(supplier);
+  }
+
+  getHigherBillOrderDataBySupplier(supplier: String) {
+    this.type = "HigherBill";
+    return this.http.get('http://localhost:4000/procument/placeOrder/' + this.type + '/supplier/' + supplier)
+      .map((res: Response) => res.json()).subscribe(order => {
+        console.log(order);
+        this.orders = order
+      })
+  }
+
+  getHigherBillOrderDataByOrderId(orderID: String) {
+    this.type = "HigherBill";
+    return this.http.get('http://localhost:4000/procument/placeOrder/' + this.type + '/id/' + orderID)
+      .map((res: Response) => res.json()).subscribe(order => {
+        console.log(order);
+        this.orders = order
+      })
+  }
+
+  public onClickSearchByOrderId(orderID): void {
+    const id = orderID;
+    console.log(id);
+    if (id == null || id == "") {
+      this.getOrder();
+    }
+    else
+      this.getHigherBillOrderDataByOrderId(id);
+  }
+
+  public focusOutOrderIdInput(id): void {
+    const val = id;
+    console.log(val);
+    if (val == null || val == "") {
+      this.getOrder();
+    }
+
+  }
+
+  public onClickSearchByDate(date): void {
+    const val = date;
+    console.log(val);
+    if (val == null || val == "") {
+      this.getOrder();
+    }
+    else
+      this.getOrderDataByDate(val);
+  }
+
+  getOrderDataByDate(date: Date) {
+    this.type = "HigherBill";
+    return this.http.get('http://localhost:4000/procument/placeOrder/' + this.type + '/date/' + date)
+      .map((res: Response) => res.json()).subscribe(order => {
+        console.log(order);
+        this.orders = order
+      })
+  }
+
+  public focusOutDateInput(val): void {
+    const value = val;
+    console.log(value);
+    if (value == null || value == "") {
+      this.getOrder();
+    }
+
   }
 
 }
