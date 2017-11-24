ede83497f77513cf3ae9d688ed610c3e4350c589
PWithanage <psathsaranee@gmail.com>
2017-11-24 12:21:59 +0530

    search supplier

--- src/app/dashboard/supplier/view-supplier/view-supplier.component.ts
+++ src/app/dashboard/supplier/view-supplier/view-supplier.component.ts
@@ -16,23 +16,24 @@ export class ViewSupplierComponent implements OnInit {
   //this function is automatically called when the specific page is loaded
   ngOnInit() {
     //calling the two functions to extract data
-    this.getSupplierData();
-    this.getSupplier();
+    this.getSuppliersData();
+    this.getSuppliers();
   }
 
   //getting Supplier data from the GET request
-  getSupplierData() {
+  getSuppliersData() {
     return this.http.get('http://localhost:4000/procument/supplier')
       .map((res: Response) => res.json())
   }
 
   //adding data to suppliers variable (defined above)
-  getSupplier() {
-    this.getSupplierData().subscribe(supplier => {
+  getSuppliers() {
+    this.getSuppliersData().subscribe(supplier => {
       console.log(supplier);
       this.suppliers = supplier
     })
   }
+
 
 
 }
