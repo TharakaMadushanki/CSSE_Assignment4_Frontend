551ed2585f997ca4cca36886e5d7dc2d674d3db4
PWithanage <psathsaranee@gmail.com>
2017-11-24 13:34:16 +0530

    search supplier done

--- src/app/dashboard/supplier/view-supplier/view-supplier.component.ts
+++ src/app/dashboard/supplier/view-supplier/view-supplier.component.ts
@@ -11,6 +11,7 @@ import 'rxjs/add/operator/map';
 export class ViewSupplierComponent implements OnInit {
 
   suppliers: any = [];
+  suppliersSearch = [];
 
   constructor(private http: Http) { }
   //this function is automatically called when the specific page is loaded
@@ -18,6 +19,7 @@ export class ViewSupplierComponent implements OnInit {
     //calling the two functions to extract data
     this.getSuppliersData();
     this.getSuppliers();
+    this.getSuppliersForSearchField();
   }
 
   //getting Supplier data from the GET request
@@ -32,6 +34,32 @@ export class ViewSupplierComponent implements OnInit {
       console.log(supplier);
       this.suppliers = supplier
     })
+  }
+
+  getSuppliersForSearchField() {
+    this.getSuppliersData().subscribe(supplier => {
+      console.log(supplier);
+      this.suppliersSearch = supplier
+    })
+  }
+
+  getSupplierByName(name:String){
+    return this.http.get('http://localhost:4000/procument/supplier/name/'+name)
+    .map((res: Response)=>res.json()).subscribe(supplier=> {
+      console.log(supplier);    
+      this.suppliers = supplier
+    })
+  }
+
+  public onChangeState(event): void {
+    const name = event.target.value;
+    console.log(name);
+    if(name == null || name == ""){
+      this.getSuppliersData();
+      this.getSuppliers();     
+    }
+    else
+      this.getSupplierByName(name);
   }
 
 
