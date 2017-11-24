7ef70415140f2bb51ce5d6582dfd6c325073b711
Tharaka Madushanki <tharakamadushanki@gmail.com>
2017-11-24 12:17:01 +0530

    order search by status

--- src/app/dashboard/orders/view-order/view-order.component.ts
+++ src/app/dashboard/orders/view-order/view-order.component.ts
@@ -10,28 +10,63 @@ import { Http, Response } from '@angular/http';
 
 export class ViewOrderComponent implements OnInit {
 
+  orders : any = [];
   suppliers : any = [];
   
   constructor(private http: Http) { }
 
   ngOnInit() {
-    this.getData();
-    this.getSupplier();
+    this.getOrderData();
+    this.getSupplierData();
+    this.getOrder();
+    this. getSupplier();
+    //this.getOrderDataByStatus(val = 'val');
   }
 
   //getting data from the get request
-  getData(){
-    return this.http.get('http://localhost:4000/procument/supplier')
+  getOrderData(){
+    return this.http.get('http://localhost:4000/procument/placeOrder')
     .map((res: Response)=>res.json())
   }
 
-    //adding data to supplier variable (defined upper) which are got from getdata() 
-    getSupplier(){
-      this.getData().subscribe(supplier=> {
-        console.log(supplier);       
-        this.suppliers = supplier
-      })
-    }
+  getSupplierData(){
+    return this.http.get('http://localhost:4000/procument/supplier')
+    .map((res: Response)=>res.json())
+  }
+  //adding data to order variable (defined sm) which are got from getdata() 
+  getOrder(){
+     this.getOrderData().subscribe(order=> {
+       console.log(order);       
+      this.orders = order
+    })
+  }
 
+  getSupplier(){
+    this.getSupplierData().subscribe(supplier=> {
+      console.log(supplier);       
+      this.suppliers = supplier
+    })
+  }
+
+  
+  getOrderDataByStatus(val:String){
+    return this.http.get('http://localhost:4000/procument/placeOrder/status/'+val)
+    .map((res: Response)=>res.json()).subscribe(order=> {
+      console.log(order);       
+      this.orders = order
+    })
+  }
+
+
+  public onChangeState(event): void {  // event will give you full breif of action
+    const newVal = event.target.value;
+    console.log(newVal);
+    if(newVal == null || newVal == ""){
+      this.getOrderData();
+      this.getOrder();     
+    }
+    else
+      this.getOrderDataByStatus(newVal);
+  }
 
 }
